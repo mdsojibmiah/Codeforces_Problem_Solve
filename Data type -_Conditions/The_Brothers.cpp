a6ce35ddf833463652570eead9fd1fd1cb7cea4b
MD. SAJIB MIAH <89442408+mdsojibmiah@users.noreply.github.com>
2024-10-12 23:15:15 +0600

    Add files via upload
    String check equal

---
+++ Data type -_Conditions/The_Brothers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string F1, S1;
+	cin >> F1 >> S1;
+	string F2, S2;
+	cin >> F2 >> S2;
+	if(S1==S2){
+		cout << "ARE Brothers";
+	}
+	else 
+	{
+		cout << "NOT";
+	}
+	return 0;
+}
