196fcdd123b9a787c2dd428cac67ddcc2e9f5c3d
MD. SAJIB MIAH <89442408+mdsojibmiah@users.noreply.github.com>
2024-11-22 23:31:58 +0600

    Update Smallest_Pair.cpp

--- Array_Problem/Smallest_Pair.cpp
+++ Array_Problem/Smallest_Pair.cpp
@@ -3,23 +3,28 @@
      Email: mdsojibmiah.coder@gmail.com
      Problem Link:https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/I
 */
-
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-	int t,r,mi=INT_MAX;
-	cin>>t;
-	while(t--){
-		int n;cin>>n;
-		int a[n];
-		for(int i=0;i<n;i++)cin>>a[i];
-		for(int i=1;i<=n;i++){
-			for(int j=i+1;j<=n;j++){
-				r=a[i-1]+a[j-1]+(j-i);
-				mi=min(mi,r);
-			}
-		}
-	}
-	cout << mi;
-	return 0;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> arr[i];
+        }
+        int minSum = INT_MAX;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                int sum = arr[i] + arr[j] + (j - i);
+                if (sum < minSum) {
+                    minSum = sum;
+                }
+            }
+        }
+    	cout << minSum << endl;
+    }
+    return 0;
 }
